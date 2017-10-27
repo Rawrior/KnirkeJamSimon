0cd9d16468415a923d9e5edf597c1ab659348c53
Unknown <mr.simon3500@gmail.com>
2017-10-28 00:13:13 +0200

    Reinstalled ReSharper :ok_hand:

---
+++ KnirkeJamProjekt/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        //Move left
+        MoveFunction(KeyCode.A, -5);
+        MoveFunction(KeyCode.LeftArrow, -5);
+        //Move right
+        MoveFunction(KeyCode.D, 5);
+        MoveFunction(KeyCode.RightArrow, 5);
+    }
+
+    void MoveFunction(KeyCode key, float speed)
+    {
+        //Do the boogie~
+        if (Input.GetKey(key))
+            transform.Translate(speed, 0, 0);
+    }
+}
