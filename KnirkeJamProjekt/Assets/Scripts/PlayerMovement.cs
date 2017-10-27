a12eafd84d14e7fb2dc66304f7980f10b20ef29d
Unknown <mr.simon3500@gmail.com>
2017-10-28 00:44:17 +0200

    Movement can't go sanic fast now. Next up, interaction!

--- KnirkeJamProjekt/Assets/Scripts/PlayerMovement.cs
+++ KnirkeJamProjekt/Assets/Scripts/PlayerMovement.cs
@@ -14,18 +14,23 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        //Move left
-        MoveFunction(KeyCode.A, -5);
-        MoveFunction(KeyCode.LeftArrow, -5);
-        //Move right
-        MoveFunction(KeyCode.D, 5);
-        MoveFunction(KeyCode.RightArrow, 5);
+        //Move left. Also make sure we can't move double speed.
+        if (Input.GetKey(KeyCode.A)) MoveFunction(KeyCode.A, -speed);
+        else if (Input.GetKey(KeyCode.LeftArrow)) MoveFunction(KeyCode.LeftArrow, -speed);
+        //Move right. Chill out bruh, go slow :~)
+	    if (Input.GetKey(KeyCode.D)) MoveFunction(KeyCode.D, speed);
+	    else if (Input.GetKey(KeyCode.RightArrow)) MoveFunction(KeyCode.RightArrow, speed);
     }
 
     void MoveFunction(KeyCode key, float speed)
     {
         //Do the boogie~
         if (Input.GetKey(key))
-            transform.Translate(speed, 0, 0);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    void UseFunction()
+    {
+        
     }
 }
