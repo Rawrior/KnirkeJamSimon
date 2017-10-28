3a84b24898faa798889148a687a3a3a8373c15d1
Unknown <mr.simon3500@gmail.com>
2017-10-28 04:17:12 +0200

    :fire: :fire: :fire: FIYAAAAHHHH! :fire: :fire: :fire:

---
+++ KnirkeJamProjekt/Assets/Scripts/FirePulser.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirePulser : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.localScale = new Vector3(Mathf.Abs(Mathf.Sin(Time.timeSinceLevelLoad))+1f, Mathf.Abs(Mathf.Sin(Time.timeSinceLevelLoad))+1f, 0);
+	}
+}
