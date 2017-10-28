7d14a6a3ff77ecc17733be0a1701e5457da42a06
Unknown <mr.simon3500@gmail.com>
2017-10-28 15:09:07 +0200

    Whoops?

--- KnirkeJamProjekt/Assets/Scripts/GamestateManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GamestateManager : MonoBehaviour
-{
-    public bool fadeInDone = false;
-    public bool movedLeft = false;
-    public bool movedRight = false;
-}
